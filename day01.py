8715a8bb1560969c683d53027c44805b999491b5
Nejc Zajc <nz3928@student.uni-lj.si>
2022-12-01 06:16:37 +0100

    day 01 :/

--- day01.py
+++ day01.py
@@ -1,3 +1,27 @@
 import sys
 file = sys.stdin.read().split("\n")
 
+c = 0
+m_a = 0
+m_b = [0, 0, 0]
+
+
+for e in file:
+    
+    if e != "":
+        c += int(e)
+
+    
+    if e == "":
+        # a)
+        if c > m_a:
+            m_a = c
+        # b)
+        if c > m_b[-1]:
+            m_b.append(c)
+            m_b.sort(reverse=True)
+            m_b.pop()
+        c = 0
+
+print("a: ", m_a)
+print("b: ", sum(m_b))
