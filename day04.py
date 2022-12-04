836513ae02572cedfed39072a690112d59d42c61
nejc.zajc.99@gmail.com <nejc.zajc.99@gmail.com>
2022-12-04 06:10:57 +0100

    day 04

--- day04.py
+++ day04.py
@@ -3,4 +3,24 @@ file = sys.stdin.read().split("\n")
 if file[-1] == "":
     file.pop()
 
-print(file)
+full = 0
+overlap = 0
+
+for pair in file:
+    a, b = pair.split(",")
+    x, y = map(int, a.split("-"))
+    z, w = map(int, b.split("-"))
+
+    if x <= z and y >= w:
+        full += 1
+    elif x >= z and y <= w:
+        full += 1
+
+    for i in range(x, y+1):
+        if i >= z and i <= w:
+            overlap += 1
+            break
+
+
+print("a: ", full)
+print("b: ", overlap)
