7b33191eb41d24d48d5cda3e53a806d1079c36b1
Nejc Zajc <nz3928@student.uni-lj.si>
2022-12-06 06:08:40 +0100

    day 06

--- day06.py
+++ day06.py
@@ -3,4 +3,28 @@ file = sys.stdin.read().split("\n")
 if file[-1] == "":
     file.pop()
 
-print(file)
+data = file[0]
+
+last4 = ""
+last14 = ""
+doneA = False
+
+i = 1
+for a in data:
+    last4 += a
+    last14 += a
+
+    if len(last4) > 4:
+        last4 = last4[1:]
+    if len(last14) > 14:
+        last14 = last14[1:]
+    
+    if len(set(last4)) == 4 and not doneA:
+        print("a: ", i)
+        doneA = True
+
+    if len(set(last14)) == 14:
+        print("b: ", i)
+        break
+
+    i += 1
