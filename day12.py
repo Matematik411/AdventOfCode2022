36987908a946d1fe9fb23e4c510edb03df52a38b
Nejc Zajc <nz3928@student.uni-lj.si>
2022-12-12 06:31:39 +0100

    day 12

--- day12.py
+++ day12.py
@@ -3,4 +3,58 @@ file = sys.stdin.read().split("\n")
 if file[-1] == "":
     file.pop()
 
-print(file)
+h = len(file)
+w = len(file[0])
+
+# I outline the grid with characters higher than the field, we the path never goes there
+grid = [["ž" for _ in range(w+2)]]
+for row in file:
+    grid.append(["ž"] + [x for x in row] + ["ž"])
+grid.append(["ž" for _ in range(w+2)])
+
+for j in range(len(grid)):
+    for i in range(len(grid[0])):
+        if grid[j][i] == "S":
+            grid[j][i] = "a"
+            start = (j, i)
+        if grid[j][i] == "E":
+            grid[j][i] = "z"
+            goal = (j, i)
+        
+dirs = [[1, 0], [0, -1], [-1, 0], [0, 1]]
+
+# b) initialized on the solution of part a)
+min_walk = 412
+
+for j_s in range(len(grid)):
+    for i_s in range(len(grid[0])):
+        if grid[j_s][i_s] == "a":
+            
+            visited = set()
+            pq = []
+            pq.append([0, (j_s, i_s)]) 
+            while pq:
+                d, c = pq.pop(0)
+
+                if c == goal:
+                    if (j_s, i_s) == start:
+                        print("a: ", d)
+
+                    min_walk = min(min_walk, d)
+                    break
+
+                if c in visited:
+                    continue
+
+                visited.add(c)
+
+                for dir in dirs:
+                    y_n = c[0] + dir[0]
+                    x_n = c[1] + dir[1]
+
+                    if ord(grid[y_n][x_n]) <= (ord(grid[c[0]][c[1]]) + 1): 
+                        pq.append((d+1, (y_n, x_n)))
+                
+                pq.sort()
+
+print("b: ", min_walk)
